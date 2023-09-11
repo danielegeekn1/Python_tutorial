daf2835a2d783a2c611b5bd690630e1f1f72fa1b
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-11 20:50:57 +0200

    working with strings variables ex

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ character_age = "35"
 character_age_n = 40
 is_present = True
 is_female = False
+phrase = 'Eichiro Oda is the best'
 print('there once was a man named' +' ' + character_name + ',' + 'and he is' + ' ' + character_age + ' ' + 'years old')
-
+print('i am\n a new boy')
+print(phrase.lower() + ' ' +'mangaka in the world')
+print(phrase.upper())
