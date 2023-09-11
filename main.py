674fa843f4a137f9dbd0ae31334efd21cb6ccc7d
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-11 20:36:08 +0200

    add variables examples

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ character_name = "John"
 character_age = "35"
 character_age_n = 40
 is_present = True
+is_female = False
 print('there once was a man named' +' ' + character_name + ',' + 'and he is' + ' ' + character_age + ' ' + 'years old')
+
