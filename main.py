7dc307adb535048a741c605a8e87de7c486705e6
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-12 19:44:11 +0200

    added an array of number to a string array

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 friends = ['Federica', 'Muslim', 'Gennaro', 'Radim']
+lucky_numbers = [3, 9, 10, 34, 12, 6]
 print(friends[1:3])
 print(friends[-1])
 print(friends[0])
 
 friends[-1] = 'Francesco'
+print(friends)
+friends.extend(lucky_numbers)
 print(friends)
