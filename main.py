dab8d37643812b674013765e444b378ea8cd38a3
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-12 21:29:25 +0200

    function with comparison and if statement

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ elif is_male and not(is_tall):
     print('you are a short male')
 else:
     print('you are a female and you are not tall')
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+    
+print(max_num(4, 6, 9))
