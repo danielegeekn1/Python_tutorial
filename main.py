5f814b5a466e87c8bca6069fdfec116f8dfb1a11
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-12 20:05:13 +0200

    first python functioN

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-coordinates= (4, 5)
-print(coordinates[0])
-#we cannot modify tuples
-
-#i could create an array of tuples instead
-coordinates_list = [(4, 5), (6, 9), (10, 17)]
-print(coordinates_list[0])
+def sayhi(): 
+    print('Hello user')
+sayhi()
