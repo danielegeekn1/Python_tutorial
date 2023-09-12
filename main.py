9015b6bc0e6dd5c927ffe8c985d1fcfd62412458
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-12 21:26:05 +0200

    other example with if statement and not operator

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ is_male = True
 is_tall = False
 if is_male or is_tall:
     print('You are a male')
+elif is_male and not(is_tall):
+    print('you are a short male')
 else:
     print('you are a female and you are not tall')
