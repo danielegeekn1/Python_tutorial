8d58c26ae49eab6ded378fe9434bd6fa3d52149d
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-12 21:24:14 +0200

    example with if and else statement and or operator

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-def sayhi(): 
-    print('Hello user')
-sayhi()
-def greeting(name):
-    print('Hello' + ' ' + name)
-greeting('Daniele')
-def best_manga_authors(name, age):
-    print('Best manga artist is' + ' ' + name + ' and He is ' + ' ' + str(age) + ' ' + 'years old')
-best_manga_authors('Eichiro', 40)
-
-def cube(num):
-    return num*num*num
-print(cube(4))
+is_male = True
+is_tall = False
+if is_male or is_tall:
+    print('You are a male')
+else:
+    print('you are a female and you are not tall')
