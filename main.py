0190812155d8c4a90c357d01bb93654633ef9d51
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-12 20:11:48 +0200

    funciton with more paramaterb, both string and numeric

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ sayhi()
 def greeting(name):
     print('Hello' + ' ' + name)
 greeting('Daniele')
+def best_manga_authors(name, age):
+    print('Best manga artist is' + ' ' + name + ' and He is ' + ' ' + str(age) + ' ' + 'years old')
+best_manga_authors('Eichiro', 40)
