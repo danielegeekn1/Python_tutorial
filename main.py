23f67bfc8757b50331b2212da5f62a782d7dae4a
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-12 19:18:55 +0200

    first input retrieved from user in python

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-character_name = "John"
-character_age = "35"
-character_age_n = 40
-is_present = True
-is_female = False
-phrase = 'Eichiro Oda is the best'
-print('there once was a man named' +' ' + character_name + ',' + 'and he is' + ' ' + character_age + ' ' + 'years old')
-print('i am\n a new boy')
-print(phrase.lower() + ' ' +'mangaka in the world')
-print(phrase.upper())
-
-print(3*4.5)
-print(10 % 3)
-
-my_age = 34
-my_num = 5
-print(str(my_age) +' '+ 'is my age')
-# not able to join number variable with string, before i had to convert my_age to string, to add to it another string 
-print(abs(my_num))
-print(pow(3, 2))
-print(max(4, 9))
-print(round(3.7))
-
-from math import *
-calories = 36.8
-print(floor(calories))
-print(ceil(calories))
-print(sqrt(36))
+name = input("Enter your name :")
+age = input('Enter your age :')
+print('Hello' + ' ' + name + 'you are' + ' ' + age + ' ' + 'years old')
