6c27ab44783ded6f6db1e26012bf41a209e037a3
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-12 21:19:33 +0200

    function with return statement

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ greeting('Daniele')
 def best_manga_authors(name, age):
     print('Best manga artist is' + ' ' + name + ' and He is ' + ' ' + str(age) + ' ' + 'years old')
 best_manga_authors('Eichiro', 40)
+
+def cube(num):
+    return num*num*num
+print(cube(4))
