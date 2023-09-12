580aaf126949317f1ab2989a1637a743d2297376
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-12 19:13:58 +0200

    add some built in math functions in python

--- main.py
+++ main.py
@@ -13,5 +13,16 @@ print(3*4.5)
 print(10 % 3)
 
 my_age = 34
+my_num = 5
 print(str(my_age) +' '+ 'is my age')
 # not able to join number variable with string, before i had to convert my_age to string, to add to it another string 
+print(abs(my_num))
+print(pow(3, 2))
+print(max(4, 9))
+print(round(3.7))
+
+from math import *
+calories = 36.8
+print(floor(calories))
+print(ceil(calories))
+print(sqrt(36))
