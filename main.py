10092b854f101ec540d58461bf0dd772850321aa
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-12 21:33:41 +0200

    first python dictionary

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-is_male = True
-is_tall = False
-if is_male or is_tall:
-    print('You are a male')
-elif is_male and not(is_tall):
-    print('you are a short male')
-else:
-    print('you are a female and you are not tall')
-
-def max_num(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        return num1
-    elif num2 >= num1 and num2 >= num3:
-        return num2
-    else:
-        return num3
-    
-print(max_num(4, 6, 9))
+calendar_months = {
+    'Jan':'January',
+    'Feb':'February',
+    'Mar':'March',
+    'Apr':'April',
+    'May':'May',
+    'Jun':'June',
+    'Jul':'July',
+    'Aug':'August',
+    'Sep':'September',
+    'Opt':'Optober',
+    'Nov':'November',
+    'Dec':'December'
+}
+print(calendar_months.get('Dec'))
