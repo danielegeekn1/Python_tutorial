03af5b1f2869f39c89063e1c2371bde67e7dea0d
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-13 08:58:00 +0200

    function with for loop within it

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ for ani in anime:
     print(ani)
 for index in range(len(anime)):
     print(index)
+
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return result
+print(raise_to_power(3, 2))
