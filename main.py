715a24be8a6b9fc368682bc7bb02d063cbc86811
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-13 08:54:10 +0200

    for loop example

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ i = 0
 while i <= 10:
     i = i + 1
     print(i)
+
+anime = ['Demon Slayer', 'One Piece', 'Naruto', 'Dragon Ball']
+for ani in anime:
+    print(ani)
+for index in range(len(anime)):
+    print(index)
