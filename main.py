c6ac5aea5a41db91ba858f874cdd6a816a9e4da7
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-14 08:52:23 +0200

    try catch example

--- main.py
+++ main.py
@@ -1,28 +1,5 @@
-i = 0
-while i <= 10:
-    i = i + 1
-    print(i)
-
-anime = ['Demon Slayer', 'One Piece', 'Naruto', 'Dragon Ball']
-for ani in anime:
-    print(ani)
-for index in range(len(anime)):
-    print(index)
-
-def raise_to_power(base_num, pow_num):
-    result = 1
-    for index in range(pow_num):
-        result = result * base_num
-    return result
-print(raise_to_power(3, 2))
-
-number_grid = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7,8, 9],
-    [0]
-]
-print(number_grid[1][1])
-for row in number_grid:
-    for col in row:
-        print(col)
+try:
+    number = int(input('Enter a number: '))
+    print(number)
+except:
+    print('invalid input')
