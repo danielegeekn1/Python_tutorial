df573ea4836f421d89d031b93148d5fbbc4ba31b
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-14 20:52:16 +0200

    added my lil multiple choice quiz

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from student import Student 
-student1 = Student("Jim", "Business", 3.1, False)
-
-print(student1.name)
+from question import Question
+question_prompts = [
+    "What colour are apples?\n(a) Red/Green\n(b Purple\n(c) Orange\n\n",
+    "What colour are bananas?\n(a) Teal\n(b Magenta\n(c) Yellow\n\n",
+    "What colour are strawberries?\n(a) Yellow\n(b Red\n(c) Blue\n\n",
+]
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "b"),
+    Question(question_prompts[2], "c"),
+]
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print('you got ' + str(score) + "/" + str(len(questions)) + 'correct')
+run_test(questions)
