a62105d290be048396dfd495cd679fde8943b198
Daniele Basile <daniele.basile.job@gmail.com>
2023-09-14 08:57:07 +0200

    ex

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 try:
+    value = 10 / 0
     number = int(input('Enter a number: '))
     print(number)
-except ZeroDivisionError:
-    print('divided by zero')
+except ZeroDivisionError as err:
+    print(err, 'divided by zero')
 except ValueError:
     print('invalid input')
